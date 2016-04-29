98f8e4518e844ba648d9d9f53e61642ce6e538ad
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2016-04-30 07:54:33 +0800

    fix

--- py/capture/views.py
+++ py/capture/views.py
@@ -49,6 +49,7 @@ def parse_file(file):
             new_user = DaySpringUser()
             # update info
             new_user.address = row['Add 1']
+            split_names = row['Name'].split(" ")
             new_user.first_name = split_names[0]
             if len(split_names) > 1:
                 new_user.last_name = split_names[1]
